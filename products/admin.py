4b86ca2f6d9718fe293109b51cce540c25b3784d
aleksmn <al-mn@yandex.ru>
2024-12-30 12:36:52 +0300

    add models

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Product
 
-# Register your models here.
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title", "description", "price"]
+    ordering = ["-price"]
+
+# # Register your models here.
+# admin.site.register(Product, ProductAdmin)
+
--- products/models.py
+++ products/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    title = models.TextField()
-    description = models.TextField()
-    price = models.TextField()
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True, default="No description")
+    price = models.DecimalField(max_digits=10, decimal_places=2) # 99 999 999.99
+    featured = models.BooleanField(default=False)
