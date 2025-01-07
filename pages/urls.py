a80ca266c9ff02ae46524fb49f67b96b40b1b157
aleksmn <al-mn@yandex.ru>
2025-01-07 12:38:01 +0300

    add views

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.home, name="home"),
+    path("home/", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("contacts/", views.contacts, name="contacts"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Привет, главная страница!")
+def home(request):
+    print(request.user)
+    return HttpResponse("Привет, это главная страница!")
+
+def about(request):
+    return HttpResponse("О проекте")
+
+def contacts(request):
+    return HttpResponse("Контакты")
